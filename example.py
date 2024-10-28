d8181eeb731591b637614fdc97c9baf0fbfba9b4
mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2024-10-28 18:15:27 -0400

    c

--- example.py
+++ example.py
@@ -86,9 +86,7 @@ for model_name in model_lists:
         print( f"Runing Time: {end_time - start_time:.2f} sec" )
         print( "-----------------------------------\n" )
 
-    datasets = ["narrativeqa", "qasper", "multifieldqa_en", "multifieldqa_zh", "hotpotqa", "2wikimqa", "musique", \
-            "dureader", "gov_report", "qmsum", "multi_news", "vcsum", "trec", "triviaqa", "samsum", "lsht", \
-            "passage_count", "passage_retrieval_en", "passage_retrieval_zh", "lcc", "repobench-p"]
+    datasets = ["qasper"]
     for dataset in datasets:
         data = load_dataset('THUDM/LongBench', dataset, split='test')
         input_ids = tokenizer(data['context'] + data["input"], return_tensors="pt").input_ids.cuda()
