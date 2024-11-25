2118071ff744f1818d3d706b7bbe7f0c1d30d2e1
mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2024-11-25 14:41:02 -0800

    c

--- self_extend_patch/Mistral.py
+++ self_extend_patch/Mistral.py
@@ -60,10 +60,12 @@ def apply_grouped_rotary_pos_emb(q, k, cos, sin, position_ids, g_size_1=1, g_siz
     return q_embed, k_embed
 '''
 
-def apply_grouped_rotary_pos_emb(q, k, cos, sin, position_ids, g_size_1=1, g_size_2=4096):
+def apply_grouped_rotary_pos_emb(q, k, cos, sin, position_ids, device, g_size_1=1, g_size_2=4096):
     # The first two dimensions of cos and sin are always 1, so we can `squeeze` them.
     #position_ids_q = position_ids//g_size_1 + g_size_2 - g_size_2//g_size_1
     #position_ids_k = position_ids//g_size_1
+
+    
     
     
     position_ids_q, position_ids_k = generate_logistically_grouping_position(position_ids.shape[1], g_size_1, device=device)
@@ -132,12 +134,13 @@ def self_extend_forward(
     # only consider bsz=1 for now. 
     key_position = torch.arange(kv_seq_len, dtype=position_ids.dtype).to(query_position.device).view(1, kv_seq_len)
 
+    device = value_states.device
 
     neighbor_query_states, _ = apply_rotary_pos_emb(scaled_query, None, cos, sin, query_position) 
     _, neighbor_key_states = apply_rotary_pos_emb(None, key_states, cos, sin, key_position) 
     _re_group_size_2 = 0 if position_ids.max() < group_size_2 else group_size_2 # in case that, the smallest q position, g2-g2//g1 exceed the max position
-    group_query_states, _ = apply_grouped_rotary_pos_emb(scaled_query, None, cos, sin, query_position, g_size_1=group_size_1, g_size_2=_re_group_size_2) 
-    _, group_key_states = apply_grouped_rotary_pos_emb(None, key_states, cos, sin, key_position, g_size_1=group_size_1, g_size_2=_re_group_size_2) 
+    group_query_states, _ = apply_grouped_rotary_pos_emb(scaled_query, None, cos, sin, query_position, device=device, g_size_1=group_size_1, g_size_2=_re_group_size_2) 
+    _, group_key_states = apply_grouped_rotary_pos_emb(None, key_states, cos, sin, key_position, device=device, g_size_1=group_size_1, g_size_2=_re_group_size_2) 
 
 
     group_key_states = repeat_kv(group_key_states, self.num_key_value_groups)
@@ -288,8 +291,10 @@ def flash_self_extend_forward(
         neighbor_query_states, _ = apply_rotary_pos_emb(scaled_query, None, cos, sin, query_position) 
         _, neighbor_key_states = apply_rotary_pos_emb(None, key_states, cos, sin, key_position) 
 
-        group_query_states, _ = apply_grouped_rotary_pos_emb(scaled_query, None, cos, sin, query_position, g_size_1=group_size_1, g_size_2=_re_group_size_2) 
-        _, group_key_states = apply_grouped_rotary_pos_emb(None, key_states, cos, sin, key_position, g_size_1=group_size_1, g_size_2=_re_group_size_2) 
+        device = value_states.device
+
+        group_query_states, _ = apply_grouped_rotary_pos_emb(scaled_query, None, cos, sin, query_position, device=device, g_size_1=group_size_1, g_size_2=_re_group_size_2) 
+        _, group_key_states = apply_grouped_rotary_pos_emb(None, key_states, cos, sin, key_position, device=device, g_size_1=group_size_1, g_size_2=_re_group_size_2) 
 
 
         neighbor_query_states = neighbor_query_states.transpose(1, 2).contiguous()
