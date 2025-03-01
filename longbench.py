24e02b6b8c2dd1080789739f5ee2505b870a7927
mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2025-03-01 14:55:44 -0500

    length limit

--- longbench.py
+++ longbench.py
@@ -235,6 +235,9 @@ for model_name in model_lists:
                 d = data['choice_D'][i]
                 #expected_answers = data["answers"][i]
                 prompt = template.replace("$DOC$", context).replace("$Q$", question).replace("$C_A$", a).replace("$C_B$", b).replace("$C_C$", c).replace("$C_D$", d)
+
+                if len(prompt) > 4096:
+                    continue
                 
                 input_ids = tokenizer(prompt, return_tensors="pt").input_ids.cuda()
                 with torch.no_grad():
