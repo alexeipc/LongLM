45606537731b21752ea57dda7f96f617f513f312
mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2025-04-29 17:41:05 -0400

    new

--- self_extend_patch/Gemma.py
+++ self_extend_patch/Gemma.py
@@ -7,6 +7,8 @@ import math
 from typing import Optional, Tuple
 
 from .selfextend_flash_attn import self_extend_flash_forward
+from .attn_method import generate_sequentially_grouping_position, generate_exponentially_grouping_position, generate_logistically_grouping_position
+
 
 from transformers.utils import is_flash_attn_2_available
 if is_flash_attn_2_available():
@@ -119,7 +121,15 @@ def flash_self_extend_forward(
     if q_len == 1:
         neighbor_key_position = position_ids[:, -1] - key_position
         _re_group_size_2 = 0 if position_ids.max() < group_size_2 else group_size_2
-        group_key_position = position_ids[:, -1]//group_size_1 - key_position//group_size_1 + (_re_group_size_2 - _re_group_size_2//group_size_1)
+        
+        #device = value_states.device
+        #group_key_position = generate_logistically_grouping_position(key_position.shape[1], group_size_2, device=device, qlen_1 = True)
+        
+        #group_key_position = position_ids[:, -1]//group_size_1 - key_position//group_size_1 + (_re_group_size_2 - _re_group_size_2//group_size_1)
+        
+        device = value_states.device
+        group_key_position = generate_logistically_grouping_position(key_position.shape[1], group_size_2, device=device, qlen_1 = True)
+        
         decode_key_position = torch.cat([group_key_position[:, :-group_size_2], neighbor_key_position[:,-group_size_2:]], dim=1)
         
         decode_k_cos, decode_k_sin = self.rotary_emb(value_states, decode_key_position, seq_len=None)
@@ -141,8 +151,16 @@ def flash_self_extend_forward(
 
 
         _re_group_size_2 = 0 if query_position.max() < group_size_2 else group_size_2 # in case that, the smallest q position, g2-g2//g1 exceed the max position
-        group_query_position = query_position // group_size_1 + _re_group_size_2 - _re_group_size_2 / group_size_1
-        group_key_position = key_position // group_size_1
+        
+        device = value_states.device
+        group_query_position, group_key_position = generate_logistically_grouping_position(query_position.shape[1], group_size_2, device=device)
+        
+        #group_query_position = query_position // group_size_1 + _re_group_size_2 - _re_group_size_2 / group_size_1
+        #group_key_position = key_position // group_size_1
+        
+        #device = value_states.device
+        #group_query_position, group_key_position = generate_logistically_grouping_position(query_position.shape[1], group_size_2, device=device)
+
 
         group_q_cos, group_q_sin = self.rotary_emb(value_states, group_query_position, seq_len=None)
         group_k_cos, group_k_sin = self.rotary_emb(value_states, group_key_position, seq_len=None)
---
+++ self_extend_patch/attn_method/logistic.cu
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include <torch/extension.h>
+
+const int MAX_THREAD = 1000;
+
+__device__ double gs_inverse_generating_function(int y, double rate, double capacity) {
+    double numerator = std::log(y * capacity - y) - std::log(capacity - y);
+    double denominator = rate;
+    return (double) numerator / denominator;
+}
+
+struct Group {
+    int first;
+    int last;
+
+    __device__ Group(int first, int last) 
+        : first(first), last(last) // Use initializer list
+    {
+    }
+};
+
+__device__ void group_id(int id, int n, int capacity, int presum, int last_group_size, Group* group, int* res) {
+    int next_group_size = last_group_size + 1;
+    int last_group_pos = group[last_group_size].last;
+    
+    int group_id = last_group_pos + (id - presum + next_group_size - 1) / next_group_size;
+    
+    if (n - id >= 0 && n >= group_id) res[n - id] = n - group_id;
+}
+
+__global__ void gpu_key_group_id(int n, int capacity, int presum, int last_group_size, Group* groups, int* res) {
+    int id = presum + threadIdx.x + blockIdx.x * blockDim.x;
+    int max_n = presum + groups[last_group_size + 1].last - groups[last_group_size + 1].first;
+	
+	if (id < n) group_id(id + 1, n, capacity, presum, last_group_size, groups, res);
+}
+
+__global__ void gpu_query_group_id(int n, int window_size, int* group_query_position, int* group_key_position) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if (i < window_size) group_query_position[i] = window_size;
+    else if (i < n) {
+        group_query_position[i] = window_size + group_key_position[i - window_size];
+    }
+}
+
+__global__ void freq_group(int capacity, double rate, Group* groups) {
+	int i = threadIdx.x + blockIdx.x * blockDim.x;
+	
+	if (i >= 1 && i < capacity - 1) {
+		double lower_bound = gs_inverse_generating_function(i, rate, capacity);
+        double upper_bound = gs_inverse_generating_function(i + 1, rate, capacity);
+
+		groups[i] = Group(ceil(lower_bound), floor(upper_bound));
+
+		if (upper_bound == (double)floor(upper_bound)) groups[i].last--;
+   }
+}
+
+void async_generator(torch::Tensor group_query_position, torch::Tensor group_key_position, int n, int window_size, double rate, double capacity) {
+    //printf("%d\n", window_size);
+	Group* groups;
+
+	cudaMallocManaged(&groups, capacity * sizeof(Group));
+
+	freq_group<<<1, capacity - 1>>>(capacity, rate, groups);
+	cudaDeviceSynchronize();
+	groups[0].last = -1;
+
+    int presum = 0;
+	for (int i = 1; i < capacity - 1; ++i) {
+        int group_size = groups[i].last - groups[i].first + 1;
+        int next_group_size = groups[i + 1].last - groups[i + 1].first + 1;
+        gpu_key_group_id<<<(n + MAX_THREAD - 1)/MAX_THREAD, MAX_THREAD>>>(n, capacity, presum, i - 1, groups ,group_key_position.data_ptr<int>());
+        presum = presum + i * group_size;
+    }
+	cudaDeviceSynchronize();
+
+    gpu_query_group_id<<<(n + MAX_THREAD - 1)/MAX_THREAD, MAX_THREAD>>>(n, window_size, group_query_position.data_ptr<int>() , group_key_position.data_ptr<int>());
+    cudaDeviceSynchronize();
+    cudaFree(groups);
+}
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("async_generator", &async_generator, "Description of your function");
+}
