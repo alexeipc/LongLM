1f71c26b0e3dbd1b3b253f947c3fa9704058ba90
mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2025-05-01 16:50:22 -0400

    13

--- longbench.py
+++ longbench.py
@@ -53,7 +53,7 @@ group_size = 32
 use_flash = True
 
 # model_lists = ['google/gemma-7b-it', 'meta-llama/Llama-2-7b-chat-hf', 'mistralai/Mistral-7B-Instruct-v0.1', ]
-model_lists = ['meta-llama/Llama-2-7b-chat-hf']
+model_lists = ['meta-llama/Llama-2-13b-chat-hf']
 auth_token = args.auth_token
 
 
