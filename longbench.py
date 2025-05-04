76d781971f3172eb3cf58380413a8a8f5f6c9f4b
mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2025-05-04 11:30:32 -0400

    mistral

--- longbench.py
+++ longbench.py
@@ -53,7 +53,7 @@ group_size = 32
 use_flash = True
 
 # model_lists = ['google/gemma-7b-it', 'meta-llama/Llama-2-7b-chat-hf', 'mistralai/Mistral-7B-Instruct-v0.1', ]
-model_lists = ['meta-llama/Llama-2-7b-chat-hf']
+model_lists = ['mistralai/Mistral-7B-Instruct-v0.3']
 auth_token = args.auth_token
 
 def process_math(response):
